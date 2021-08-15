cf2e28be7385f7f1c7944da17fe0b6ffb54f5e10
nugroho <nugroho@mackbookpro.local>
2021-08-15 10:34:50 +0700

    Initial | Change get service

--- src/get/main.go
+++ src/get/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strconv"
 
 	healthcheck "github.com/RaMin0/gin-health-check"
 	"github.com/gin-gonic/gin"
@@ -13,11 +14,18 @@ func main() {
 		panic(err)
 	}
 
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	pagesize := os.Getpagesize()
+
 	r := gin.Default()
 	r.Use(healthcheck.Default())
 	r.GET("/get-ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong from " + name,
+			"message": "pong from " + name + " in " + pwd + " pagesize " + strconv.Itoa(pagesize),
 		})
 	})
 
